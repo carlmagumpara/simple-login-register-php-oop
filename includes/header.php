41196361e9937facd009a7416d4adf8eb468a05b
carlmagumpara <magumparacarlanthony@gmail.com>
2017-08-07 12:34:23 +0800

    remove vue

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,4 @@
 <link rel="stylesheet" type="text/css" href="./assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="./assets/css/main.css">
-<script src="./assets/bower_components/vue/dist/vue.min.js"></script>
 <script src="./assets/bower_components/jquery/dist/jquery.min.js"></script>
 <script src="./assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
